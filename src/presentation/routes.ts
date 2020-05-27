d7221b83e47240f85c838bbe8428965d144a3658
Igor Alves <igoralvesantos@gmail.com>
2020-05-27 18:07:22 -0300

    Add Project Architecture

---
+++ src/presentation/routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+
+
+
+export default app;
